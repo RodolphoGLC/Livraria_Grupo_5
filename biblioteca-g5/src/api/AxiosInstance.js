ead4e455ee0432562986ab65f174460d7e1df615
boubeejul <juliana.boubee@gmail.com>
2023-06-22 12:43:27 -0300

    AxiosInstance voltou a ser exportado

--- biblioteca-g5/src/api/AxiosInstance.js
+++ biblioteca-g5/src/api/AxiosInstance.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const AxiosInstance = axios.create({
+export const AxiosInstance = axios.create({
     //Colocar o seu id do PC 
     baseURL: "http://192.168.1.4:8080/api"
    // baseURL:"http://192.168.1.5:8080/api" //sant
