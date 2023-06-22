72aa795e2056f8bb4212eba8b29a5e054b5ad240
AllanFerreiraGomes <allanferreiragomes1@gmail.com>
2023-06-22 19:21:33 -0300

    css tela LivroScreen

--- biblioteca-g5/src/screen/LivroScreen.js
+++ biblioteca-g5/src/screen/LivroScreen.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image, FlatList } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
-
-const LivroScreen = ({ route }) => {
+const LivroScreen = ({ navigation,route }) => {
   const livroData = route.params
+
+  const dataLancamento = new Date(livroData.dataLancamento);
+  const dia = dataLancamento.getDate();
+  const mes = dataLancamento.getMonth() +1 ;
+  const ano = dataLancamento.getFullYear();
+  
+  const dataFormatada = `${dia}/${mes}/${ano}`;
 
   return (
     <View style={styles.container}>
@@ -14,8 +20,11 @@ const LivroScreen = ({ route }) => {
         </View>
         <View style={styles.cardInfo}>
           <Text style={styles.tituloLivro}>{livroData.nomeLivro}</Text>
-          <Text style={styles.autorLivro}>{livroData.autor}</Text>
+          <Text style={styles.autorLivro}>{livroData.autorDTO.autor}</Text>
           <Text style={styles.editoraLivro}>{livroData.editoraDTO?.nomeEditora}</Text>
+          <Text style={styles.codigoLivro}>{livroData.codigoLivro}</Text>
+          <Text style={styles.dataLancamento}>{dataFormatada}</Text>
+
         </View>
       </View>
 
@@ -23,46 +32,59 @@ const LivroScreen = ({ route }) => {
   );
 };
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#C2DEDC',
     alignItems: 'center',
     justifyContent: 'center',
   },
   cardLivro: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'space-between',
     padding: 10,
     marginVertical: 10,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 5,
-    width: '80%',
+    borderRadius: 30,
+    backgroundColor: '#ECE5C7',
+    shadowColor: '#5e5c50',
+    shadowOffset: {
+      width: 50,
+      height: 50,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 53,
+    elevation: 5,
   },
   cardImagem: {
-    marginRight: 10,
+    marginBottom: 10,
+    width: 320,
+    height: 320,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   cardInfo: {
     flex: 1,
+    alignItems: 'center',
   },
   tinyLogo: {
-    width: 50,
-    height: 50,
+    width: 250,
+    height: 300,
   },
-  tituloLivro: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 5,
-  },
-  autorLivro: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  editoraLivro: {
-    marginBottom: 5,
-  },
+  
+tituloLivro: {
+  fontSize: 25,
+  fontWeight: 'bold',
+  marginBottom: 5,
+  maxWidth: 330, 
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+},
+
+  editoraLivro:{
+    fontSize: 15,
+  }
 });
 
 export default LivroScreen;
