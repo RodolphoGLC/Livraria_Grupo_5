4a9fe23d8ce07c518bc343729df3690b60000c68
Gabriel Santiago Neves <gabrielsantiagoneves2@gmail.com>
2023-06-23 17:24:12 -0300

    login

--- biblioteca-g5/src/screen/LoginScreen.js
+++ biblioteca-g5/src/screen/LoginScreen.js
@@ -1,103 +1,132 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet,  View, Text, TextInput, TouchableOpacity } from 'react-native';
-import { useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { Ionicons } from 'react-native-vector-icons';
 import { AxiosInstance } from '../api/AxiosInstance';
 import { DataContext } from '../context/DataContext';
 
-
 export const LoginScreen = ({ navigation }) => {
+  const [usuario, setUsuario] = useState('');
+  const [senha, setSenha] = useState('');
+  const [mostrarSenha, setMostrarSenha] = useState(false);
+  const [erro, setErro] = useState('');
+  const { armazenarDadosUsuario } = useContext(DataContext);
 
-    const [usuario, setUsuario] = React.useState('');
-    const [senha, setSenha] = React.useState('');
-    const {armazenarDadosUsuario} = useContext(DataContext);
+  const handleLogin = async () => {
+    try {
+      const resultado = await AxiosInstance.post('/auth/signin', {
+        username: usuario,
+        password: senha
+      });
 
+      if (resultado.status === 200) {
+        console.log('deu certo');
+        var jwtToken = resultado.data;
+        armazenarDadosUsuario(jwtToken["accessToken"]);
 
-    const handleLogin = async () => {
-    
-        try{
-          const resultado = await AxiosInstance.post('/auth/signin',{
-            username: usuario,
-            password: senha
-          });
-    
-          if(resultado.status === 200){
-            
-            console.log('deu certo')
-            var jwtToken = resultado.data;
-            armazenarDadosUsuario(jwtToken["accessToken"]);
-    
-            navigation.navigate('Livraria')
-    
-          } else {
-            console.log('erro');
-          }
-    
-        }catch (error){
-          console.log('erro: ' + error);
-        }
+        navigation.navigate('Livraria');
+      } else {
+        console.log('erro');
+        
       }
+    } catch (error) {
+      console.log('erro: ' + error);
+      setErro('Usuário ou senha incorretos');
+    }
+  };
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.titulo}>Bem-vindo</Text>
-            <TextInput
-                style={styles.input}
-                onChangeText={setUsuario}
-                value={usuario}
-                placeholder="Usuario"
-            />
-            <TextInput
-                style={styles.input}
-                onChangeText={setSenha}
-                value={senha}
-                placeholder="Senha"
-            />
+  const alternarVisibilidadeSenha = () => {
+    setMostrarSenha(!mostrarSenha);
+  };
 
-            <TouchableOpacity style={styles.button} onPress={handleLogin}>
-                <Text style={styles.txtButton}>Entrar</Text>
-            </TouchableOpacity>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Bem-vindo</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setUsuario}
+        value={usuario}
+        placeholder="Usuário"
+      />
+      <View style={styles.senhaContainer}>
+        <TextInput
+          style={styles.inputSenha}
+          onChangeText={setSenha}
+          value={senha}
+          placeholder="Senha"
+          secureTextEntry={!mostrarSenha}
+        />
+        <TouchableOpacity style={styles.olhoButton} onPress={alternarVisibilidadeSenha}>
+          <Ionicons name={mostrarSenha ? 'eye-off' : 'eye'} size={24} color="#000" />
+        </TouchableOpacity>
+      </View>
 
-        </View>
-    );
-}
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.txtButton}>Entrar</Text>
+      </TouchableOpacity>
+
+      {erro !== '' && <Text style={styles.erro}>{erro}</Text>}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#116a7b',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    titulo: {
-        fontSize: 40,
-        fontWeight: 'bold',
-        marginVertical: 10,
-        color: '#000'
-    },
-    input: {
-        height: 40,
-        width: 270,
-        margin: 12,
-        borderWidth: 1,
-        borderRadius: 15,
-        padding: 10,
-        backgroundColor: '#cdc2ae',
-    },
-    button: {
-        borderRadius: 15,
-        backgroundColor: '#000',
-        color: '#fff',
-        width: 150,
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlignVertical: 'center',
-        fontSize: 16,
-        margin: 10,
-        paddingTop: 10,
-        paddingBottom: 10,
-    },
-    txtButton: {
-      color: '#fff',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#116a7b',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginVertical: 10,
+    color: '#000',
+  },
+  input: {
+    height: 40,
+    width: 270,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 10,
+    backgroundColor: '#cdc2ae',
+  },
+  senhaContainer: {
+    position: 'relative',
+    width: 270,
+  },
+  inputSenha: {
+    height: 40,
+    width: '100%',
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 10,
+    backgroundColor: '#cdc2ae',
+  },
+  olhoButton: {
+    position: 'absolute',
+    right: 10,
+    top: 8,
+  },
+  button: {
+    borderRadius: 15,
+    backgroundColor: '#000',
+    color: '#fff',
+    width: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlignVertical: 'center',
+    fontSize: 16,
+    margin: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  txtButton: {
+    color: '#fff',
+  },
+  erro: {
+    color: 'red',
+    marginTop: 10,
+  },
 });
